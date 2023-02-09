f99f4918c00a99277004035ce842b83f73b39c8f
VikashShahi62 <124891192+VikashShahi62@users.noreply.github.com>
2023-02-09 18:20:56 +0000

    Add files via upload

---
+++ Assesment 03/Inverted_half_pyramid.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+//WAP to print inverted pyramid
+ 
+
+public class Inverted_half_pyramid {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("* ");			
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Assesment 03/Nestedfor.java
@@ -0,0 +1,20 @@
+package JavaPractice;
+
+
+
+public class Nestedfor {
+
+	public static void main(String[] args) {
+		// Loop for i
+		for(int i=1;i<=3;i++)
+		{
+			//Loop for j
+			for(int j=1;j<=3;j++)
+			{
+				System.out.println(i+" "+j);
+			}
+		}
+
+	}
+
+}
